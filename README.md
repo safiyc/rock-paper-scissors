1cc429934240cdcbc8c108e91a7322638541f479
Safiy Cham <csafiy@yahoo.com>
2018-07-08 19:23:11 -0700

    Clean up some of the code for readability.

--- README.md
+++ README.md
@@ -2,6 +2,12 @@ This rock, paper, scissors program done in JavaScript lets a user play the popul
 
 
 need to do:
--work on html/css; most js functionality done
-nah, will TDD next project -work backward; implement jasmine TDD (for practice)
--dry code
+-DRY
+-modify code to ES6
+
+-work on html/css (rock-paper-scissors page)
+-create new html/css/js (quiz page loads after losing to cpu)
+  -quiz page is multiple choice questions (radio buttons and randomly chosen questions from a list of questions)
+
+nope (will TDD next project):
+-work backward; implement jasmine TDD (for practice)
--- script.js
+++ script.js
@@ -1,80 +1,42 @@
 const inputChoices = document.querySelectorAll(".input-choices");
 const reset = document.getElementById("reset");
+let roundDisplayed = document.getElementById("round-displayed");
 let playerScore = document.getElementById("player-score");
 let cpuScore = document.getElementById("computer-score");
-let roundDisplayed = document.getElementById("round-displayed");
 let winnerDisplay = document.getElementById("winner-display");
-
-let rounds = 0;
+let playerSelection;
+let computerSelection;
 let roundResult;
+let rounds;
 
-roundDisplayed = 1;
-playerScore = 0;
-cpuScore = 0;
 
-document.getElementById("round-displayed").innerHTML = roundDisplayed;
-document.getElementById("player-score").innerHTML = playerScore;
-document.getElementById("computer-score").innerHTML = cpuScore;
-
-inputChoices.forEach(function (button) {
-    button.addEventListener('click', function () {
-        playerSelection = button.id;
-    });
-    button.addEventListener('click', game);
-});
-
-reset.addEventListener('click', resetGame);
-
-function game () {
-    rounds++;
-
-    if (roundDisplayed >= 5) {
-        roundDisplayed = 5;
-    } else {
-        roundDisplayed++;
-    }
-
-    computerSelection = computerPlay();
+function onPageLoad () {
+    rounds = 0;
+    roundDisplayed = 1;
+    playerScore = 0;
+    cpuScore = 0;
     
-    playRound (playerSelection, computerSelection);
-    roundScore();
-    
-    winnerDisplay.classList.add("hidden");
-
-    document.getElementById("round-result").innerHTML = roundResult;
-    document.getElementById("player-move").innerHTML = playerSelection;
-    document.getElementById("computer-move").innerHTML = computerSelection;
     document.getElementById("round-displayed").innerHTML = roundDisplayed;
     document.getElementById("player-score").innerHTML = playerScore;
     document.getElementById("computer-score").innerHTML = cpuScore;
     
-    if(rounds === 5) {
-        const playerWins = "You win the game. Reset to play again.";
-        const cpuWins = "You lose the game. Answer some questions to reset the game.";
-        const draw = "It's a draw. Play another round.";
-
-        inputChoices.forEach(function (button) {
-            button.setAttribute("disabled", "disabled");
+    inputChoices.forEach(function (button) {
+        button.addEventListener('click', function () {
+            playerSelection = button.id;
         });
-        
-        winnerDisplay.classList.remove("hidden");
+        button.addEventListener('click', game);
+    });
+}
 
-        if (playerScore === cpuScore) {
-            document.getElementById("winner-display").innerHTML = draw;
+function game () {
+    rounds++;
+    winnerDisplay.classList.add("hidden");
 
-            rounds = 4;
-
-            inputChoices.forEach(function (button) {
-                button.removeAttribute("disabled", "disabled");
-            });
-        }
-        
-        if (playerScore > cpuScore) {
-            document.getElementById("winner-display").innerHTML = playerWins;
-        } else if (playerScore < cpuScore) {
-            document.getElementById("winner-display").innerHTML = cpuWins;
-        } 
-    }
+    computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+    roundScore();
+    roundCount();
+    updateDisplays();
 }
 
 function computerPlay () {
@@ -107,22 +69,43 @@ function roundScore() {
     } 
 }
 
-function resetGame () {
-    roundDisplayed = 1;
-    rounds = 0;
-    playerScore = 0;
-    cpuScore = 0;
+function roundCount () {
+    if (roundDisplayed >= 5) {
+        roundDisplayed = 5;
+    } else {
+        roundDisplayed++;
+    }
 
-    inputChoices.forEach(function (button) {
-        button.removeAttribute("disabled", "disabled");
-    });
+    if(rounds === 5) {
+        const playerWins = "You win the game. Reset to play again.";
+        const cpuWins = "You lose the game. Answer some questions to reset the game.";
+        const draw = "It's a draw. Play another round.";
 
-    roundResult = "";
-    playerSelection = "";
-    computerSelection = "";
-    
-    winnerDisplay.classList.add("hidden");
-    
+        inputChoices.forEach(function (button) {
+            button.setAttribute("disabled", "disabled");
+        });
+        
+        winnerDisplay.classList.remove("hidden");
+
+        if (playerScore === cpuScore) {
+            document.getElementById("winner-display").innerHTML = draw;
+
+            rounds = 4;
+
+            inputChoices.forEach(function (button) {
+                button.removeAttribute("disabled", "disabled");
+            });
+        }
+        
+        if (playerScore > cpuScore) {
+            document.getElementById("winner-display").innerHTML = playerWins;
+        } else if (playerScore < cpuScore) {
+            document.getElementById("winner-display").innerHTML = cpuWins;
+        } 
+    }
+}
+
+function updateDisplays () {
     document.getElementById("round-result").innerHTML = roundResult;
     document.getElementById("player-move").innerHTML = playerSelection;
     document.getElementById("computer-move").innerHTML = computerSelection;
@@ -130,3 +113,22 @@ function resetGame () {
     document.getElementById("player-score").innerHTML = playerScore;
     document.getElementById("computer-score").innerHTML = cpuScore;
 }
+
+function resetGame () {
+    roundDisplayed = 1;
+    rounds = 0;
+    playerScore = 0;
+    cpuScore = 0;
+    roundResult = "";
+    playerSelection = "";
+    computerSelection = "";
+    winnerDisplay.classList.add("hidden");
+
+    inputChoices.forEach(function (button) {
+        button.removeAttribute("disabled", "disabled");
+    });
+
+    updateDisplays();
+}
+
+reset.addEventListener('click', resetGame);
